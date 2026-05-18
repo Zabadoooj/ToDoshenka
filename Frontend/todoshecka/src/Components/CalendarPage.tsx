6e6b8c5fab0fa2213e4a593a3bdf41a9288c50a2
Zabadoooj <qastuw@gmail.com>
2026-05-18 22:01:58 +0300

    Calendar page layout

--- Frontend/todoshecka/src/Components/CalendarPage.tsx
+++ Frontend/todoshecka/src/Components/CalendarPage.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import './CalendarPage.css'
 
 import {
     ArrowLeft,
-    ArrowRight
+    ArrowRight,
+    Plus
 } from 'lucide-react'
 
 const WEEK_DAYS = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
@@ -14,12 +16,39 @@ const MONTHS_RU = [
 
 
 
+function DayItem({day}) {
+
+    return(
+        <div className="daySlot">
+            <div className="mainInfo">
+                <h1>
+                    {day}
+                </h1>
+
+                <div className="eventsList">
+                    <div className="event">Событие1</div>
+                    <div className="event">Событие2</div>
+                    <div className="event">Событие3</div>
+                </div>
+            </div>
+
+            <div className="stats">
+                <div className="notesCount">11</div>
+                <div className="ToDoCount">25</div>
+                <div className="eventsCount">2</div>
+            </div>
+        </div>
+    )
+}
+
+
 function CalendarPage() {
 
     
     const today = new Date()
-    const currentYear = today.getFullYear()
-    const currentMonth = today.getMonth()
+    const [currentYear, setYear] = useState(today.getFullYear())
+    const [currentMonth, setMonth] = useState(today.getMonth())
+    const currentDay = today.getDay()
 
     let firstDayOfMonth = new Date(currentYear, currentMonth, 1)
     let totalDaysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
@@ -31,14 +60,44 @@ function CalendarPage() {
     let daysArray = Array.from({ length: totalDaysInMonth }, (_, i) => i + 1);
 
 
+    const updateMonth = (month: any) => {
+
+        if (currentMonth - 1 != 0) {setMonth(currentMonth + month)}
+        else if (currentMonth <= 0) {updateYear(false)}
+
+        else if (currentMonth + 1 != 12) {setMonth(currentMonth + month)}
+        else if (currentMonth >= 12) {updateYear(true)}
+
+        
+        console.log(currentMonth);
+    }
+
+
+    const updateYear = (plusYear: boolean) => {
+        
+        if (plusYear) {
+            setYear(currentYear + 1)            
+            setMonth(0)
+        }
+        else{
+            setYear(currentYear - 1)
+            setMonth(0)
+        }
+        
+        console.log(currentYear, currentMonth);
+        // plusYear ? setYear(currentYear + 1) : setYear(currentYear - 1)
+        // setMonth(1)
+        
+    }
+
 
     return(
         <div className="calendarPage">
 
             <div className="centerBox">
-                {/* <div className="pattern" /> */}
+                <div className="pattern" />
                 <div className="switchMonth">
-                    <button>
+                    <button onClick={() => currentMonth - 1 != 0 ? updateMonth(-1) : updateMonth(1)}>
                         <ArrowLeft/>
                     </button>
 
@@ -46,7 +105,7 @@ function CalendarPage() {
                         {MONTHS_RU[currentMonth]} {currentYear}
                     </div>
 
-                    <button>
+                    <button onClick={() => currentMonth + 1 != 12 ? setMonth(currentMonth + 1) : setMonth(currentMonth)}>
                         <ArrowRight/>
                     </button>
                 </div>
@@ -60,28 +119,34 @@ function CalendarPage() {
             </div>
 
 
+            {/* <div className="emptySlots">
+                {emptySlots.map((emptySlot) => (
+                    <div className="emptyDay">{emptySlot}</div>  
+                ))}
+            </div> */}
+
             <div className="daysList">
+                {/* <DayItem day="1"/> */}
                 {daysArray.map((day) => (
-                    <div className="daySlot">
-                        <h1>
-                            {day}
-                        </h1>
-
-                        <div className="stats">
-                            <div className="notesCount">11</div>
-                            <div className="ToDoCount">25</div>
-                            <div className="eventsCount">2</div>
-                        </div>
-
-                        <div className="eventsList">
-                            <div className="event">#Текст</div>
-                        </div>
-                    </div>
+                    <DayItem day={day}/>
                 ))}
             </div>
+            
 
+            <div className="calendarBottom">
 
+            <hr/>
 
+            <button>
+                Повторяющееся событие
+
+                <div className="plus">
+                    <Plus />
+                </div>
+            </button>
+
+            </div>
+            
         </div>
     )
 }
